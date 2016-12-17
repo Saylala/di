aed198c0ca877087d9f83907020b1c5604ec8d4a
Saylala <boulevardoffakes@gmail.com>
2016-12-17 20:38:06 +0500

    Cloud abstraction added

---
+++ TagsCloudApp/Cloud.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Drawing;
+
+namespace TagsCloudApp
+{
+    public class Cloud<T>
+    {
+        public ImmutableDictionary<T, Rectangle> Elements { get; private set; }
+        public ImmutableDictionary<T, Color> Colors { get; private set; }
+
+        private Cloud()
+        {
+
+        }
+
+        public Cloud(Dictionary<T, Rectangle> elements)
+        {
+            Elements = elements.ToImmutableDictionary();
+        }
+
+        public Cloud<T> PaintCloud(Dictionary<T, Color> colors)
+        {
+            return new Cloud<T>
+            {
+                Elements = Elements,
+                Colors = colors.ToImmutableDictionary()
+            };
+        }
+    }
+}
